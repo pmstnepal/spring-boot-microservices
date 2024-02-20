b77b45b3eecc7975530e7325169f24631a7bc8c8
Sunny <sunnysinghssvk@gmail.com>
2024-02-20 14:31:10 +0530

    Refactored Package-Name

--- service-registry/src/main/java/com/registry/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/registry/serviceregistry/ServiceRegistryApplication.java
@@ -1,4 +1,4 @@
-package com.registry.serviceRegistry;
+package com.registry.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- service-registry/src/test/java/com/registry/serviceregistry/ServiceRegistryApplicationTests.java
+++ service-registry/src/test/java/com/registry/serviceregistry/ServiceRegistryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.registry.serviceRegistry;
+package com.registry.serviceregistry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
